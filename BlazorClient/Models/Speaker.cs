b301e20d0a34fbb9ca2292565104d7af1a156821
luca hsieh <chungju.hsieh.ca@gmail.com>
2020-03-03 14:18:15 -0800

    fix email validation

--- BlazorClient/Models/Speaker.cs
+++ BlazorClient/Models/Speaker.cs
@@ -6,18 +6,23 @@ namespace BlazorClient.Models
     {
         public string speakerId { get; set; }
         [Required]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "First Name should be alphabetical")]
         public string firstName { get; set; }
         [Required]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Last Name should be alphabetical")]
         public string lastName { get; set; }
         [Required]
+        [EmailAddress]
         public string email { get; set; }
         [Required]
+        [Phone]
         public string mobileNumber { get; set; }
         [Required]
         public string areaOfSpecialization { get; set; }
         [Required]
         public string cityOfResidence { get; set; }
         [Required]
+        [RegularExpression("^[a-zA-Z]{2}", ErrorMessage = "Province should be exact two letters e.g. BC, WA")]
         public string provinceOfResidence { get; set; }
         [Required]
         public string employer { get; set; }
